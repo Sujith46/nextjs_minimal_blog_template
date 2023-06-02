8cbd802a17b23320eea2615605bcc5b2a41d9d08
Sujith46 <sujithsuresh246@gmail.com>
2023-06-02 23:54:37 +0530

    fix: removed layout use client directive

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,3 @@
-"use client";
-
-import { GlobalStyles } from "@/commonStyled/Global.syled";
 import StyledComponentsRegistry from "@/lib/styled_registry";
 import { Manrope } from "next/font/google";
 
@@ -15,7 +12,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={manrope.className}>
-        <GlobalStyles />
         <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
       </body>
     </html>
