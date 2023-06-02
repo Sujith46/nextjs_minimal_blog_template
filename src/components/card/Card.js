62c2ce4f33b3e7c53bc02bbf1284a84bda749a28
Sujith46 <sujithsuresh246@gmail.com>
2023-06-02 13:56:15 +0530

    fix: removes scaling css

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -54,7 +54,6 @@ const CardContainer = styled(Link)`
     -webkit-box-shadow: 0px 2px 70px 0px rgba(0, 0, 0, 0.3);
     -moz-box-shadow: 0px 2px 70px 0px rgba(0, 0, 0, 0.3);
     box-shadow: 0px 2px 70px 0px rgba(0, 0, 0, 0.18);
-    scale: 1.008;
   }
 
   @media ${device.mobile} {
